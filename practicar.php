19c9ffe7972b9eaa131b1e382bbb6c719a25c13a
NachRusten <jpardo180@gmail.com>
2024-03-10 19:46:28 -0500

    practicado hasta array y objetos

---
+++ practicar.php
@@ -0,0 +1,43 @@
+<?php
+
+const SIEMPRE= "Buenos Dias";
+
+$saludo = "Hola";
+$dias = (string) 6.78; //estoy es casting
+
+$frutas = [
+    "Manzana" =>array(
+        "Nombre" => "Manzana",
+        "Color" => "Roja",
+        "Sabor" => "Dulce"
+    ), 
+    "Pera"=> array(
+        "Nombre" => "Pera",
+        "Color" => "Verde",
+        "Sabor" => "Dulce"    
+    ), 
+    "Piña" => array (
+        "Nombre" => "Piña",
+        "Color" => "Amarillo",
+        "Sabor" => "Acida"
+    ), 
+    "Naranja" => array (
+        "Nombre" => "Naranja",
+        "Color" => "Amarillo",
+        "Sabor" => "Acida"
+    )];
+
+
+$marcas = array("ferrari", "Mclaren", "Toyata", "Pegout");
+
+
+
+
+echo "La {$frutas["Manzana"]["Nombre"]} tiene un sabor {$frutas['Manzana']['Sabor']}"."\n";
+
+
+
+
+
+
+?>
