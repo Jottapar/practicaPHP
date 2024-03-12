19cb097869eb9aa4eb88b3253d5922facbdff42c
NachRusten <jpardo180@gmail.com>
2024-03-11 21:01:11 -0500

    se practica hasta array-fill_key

--- practicar.php
+++ practicar.php
@@ -31,28 +31,56 @@ $frutas = [
 
 
 
-$marcas = array(
+$marcas_a = array(
     "ferrari" => "rojo", 
     "Mclaren" => "negro", 
     "Toyata" => "blanco", 
     "Pegout" => "azul"
 );
 
+$marcas_b = [
+    "Ford" => "Azul",
+    "ferrari" => "blanco",
+    "Mclaren" => "negro"
+];
+
+$cafe = ["latte","Capucchino", "Descafeinado"];
+
+$precios = [15,22,12];
+
 echo "La {$frutas["Manzana"]["Nombre"]} tiene un sabor {$frutas['Manzana']['Sabor']}"."\n";
 
 require('functions.php');
 
+$ropa = ["camisa","pantalon", "camiseta", "pantalon"];
 
-echo Nombre("Johan","Pardo","Hombre")."\n";
+$refill = array_fill(1,4,"Refill");
+// echo Nombre("Johan","Pardo","Hombre")."\n";
 
-echo saborjugo($frutas["Manzana"]["Nombre"])."\n";
+// echo saborjugo($frutas["Manzana"]["Nombre"])."\n";
 
 
-echo contador(10)."\n";
+// echo contador(10)."\n";
 
-echo encontrarids(123456)."\n";
+// echo encontrarids(123456)."\n";
 
-echo sumarhastaten(2);
+// echo sumarhastaten(2);
 
-echo colorcarros($marcas);
+// echo colorcarros($marcas);
+
+print_r(array_change_key_case($marcas_a,CASE_UPPER)) ."\n";
+print_r(array_column($frutas,"Nombre"));
+print_r(array_chunk($frutas,2,true));
+print_r(array_combine($cafe,$precios));
+print_r(array_merge($marcas_a,$cafe));
+print_r(array_merge_recursive($marcas_a,$marcas_b));
+print_r(array_count_values($ropa));
+
+print_r(array_diff_assoc($marcas_a,$marcas_b));
+print_r(array_diff_key($marcas_a,$marcas_b));
+print_r(array_diff($marcas_a,$marcas_b));
+print_r(array_fill_keys($cafe,12));
+
+print_r($refill);
+
 ?>
